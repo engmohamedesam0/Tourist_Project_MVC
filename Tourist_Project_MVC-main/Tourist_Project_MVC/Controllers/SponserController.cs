00d82c58dad0be74f15ee6fe9aaf39e69a73f6b9
Abdelrahman <abdelrahman.ashraf.c38@gmail.com>
2026-06-30 23:17:44 +0300

    Sponsors Controler and Views

---
+++ Tourist_Project_MVC-main/Tourist_Project_MVC/Controllers/SponserController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tourist_Project_MVC.Models;
+using Tourist_Project_MVC.Repositories;
+using Tourist_Project_MVC.View_Model;
+
+namespace Tourist_Project_MVC.Controllers
+{
+    public class SponsorController : Controller
+    {
+        private readonly ISponsorRepository sponsorRepo;
+        private readonly IRewardRepository rewardsRepo;
+
+        public SponsorController(ISponsorRepository sponsorRepo , IRewardRepository rewardsRepo)
+        {
+            this.sponsorRepo = sponsorRepo;
+            this.rewardsRepo = rewardsRepo;
+        }
+        #region Index
+        public IActionResult Index()
+        {
+            IEnumerable<Sponsor> allSponsers = sponsorRepo.GetAll();
+            return View("index", allSponsers);
+        }
+        #endregion
+
+        #region Details
+        public IActionResult Details(int Id)
+        {
+            Sponsor? sponsorFromDB = sponsorRepo.GetByIdWithRewars(Id);
+            if (sponsorFromDB == null)
+            {
+                return NotFound();
+            }
+            return View("Details", sponsorFromDB);
+        }
+        #endregion
+
+        #region Edit
+        public IActionResult Edit(int id)
+        {
+            Sponsor? sponsorFromDB = sponsorRepo.GetById(id);
+            IEnumerable<Reward> allRewards = rewardsRepo.GetAll();
+            if (sponsorFromDB == null) 
+            {
+                return NotFound();
+            }
+            sponsorViewModel sponsorViewModel = new sponsorViewModel()
+            {
+                Id = sponsorFromDB.Id,
+                Name = sponsorFromDB.Name,
+                Type = sponsorFromDB.Type,
+                City = sponsorFromDB.Address,
+                ContactInfo = sponsorFromDB.ContactNumber,
+
+
+            };
+            return View("Edit", sponsorViewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(sponsorViewModel sponsorFromReq)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                Sponsor sponsorFromDB = sponsorRepo.GetById(sponsorFromReq.Id);
+                sponsorFromDB.Name = sponsorFromReq.Name;
+                sponsorFromDB.Type = sponsorFromReq.Type;
+                sponsorFromDB.Address = sponsorFromReq.City;
+                sponsorFromDB.ContactNumber = sponsorFromReq.ContactInfo;
+                sponsorRepo.Update(sponsorFromDB);
+                sponsorRepo.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Edit", sponsorFromReq);
+        }
+        #endregion
+
+        #region Create
+        public IActionResult Create()
+        {
+            sponsorViewModel createSponsorModel = new sponsorViewModel();
+            return View("Create", createSponsorModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(sponsorViewModel sponsorFromReq)
+        {
+            if (ModelState.IsValid)
+            {
+                Sponsor newSponsor = new Sponsor() 
+                {
+                    Name= sponsorFromReq.Name,
+                    Type= sponsorFromReq.Type,
+                    Address = sponsorFromReq.City,
+                    ContactNumber = sponsorFromReq.ContactInfo,
+                };
+                sponsorRepo.Add(newSponsor);
+                sponsorRepo.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Create", sponsorFromReq);
+        }
+        #endregion
+
+        #region Delete
+        public IActionResult Delete(int id)
+        {
+            Sponsor sponsorFromDB = sponsorRepo.GetById(id);
+            if (sponsorFromDB == null)
+            {
+                return NotFound();
+            }
+            sponsorViewModel deletedSponsorVM = new sponsorViewModel()
+            {
+                Id = sponsorFromDB.Id,
+                Name = sponsorFromDB.Name,
+                Type = sponsorFromDB.Type,
+                City = sponsorFromDB.Address,
+                ContactInfo = sponsorFromDB.ContactNumber,
+            };
+            return View("Delete", deletedSponsorVM);
+        }
+
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            sponsorRepo.Delete(id);
+            sponsorRepo.Save();
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+
+    }
+}
--- Tourist_Project_MVC-main/Tourist_Project_MVC/Repositories/SponsorRepository.cs
+++ Tourist_Project_MVC-main/Tourist_Project_MVC/Repositories/SponsorRepository.cs
@@ -1,10 +1,22 @@
-﻿using Tourist_Project_MVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Tourist_Project_MVC.Data;
 using Tourist_Project_MVC.Models;
 
 namespace Tourist_Project_MVC.Repositories
 {
     public class SponsorRepository : Repository<Sponsor>, ISponsorRepository
     {
-        public SponsorRepository(TouristContext context) : base(context) { }
+        private readonly TouristContext context;
+
+        public SponsorRepository(TouristContext context) : base(context)
+        {
+            this.context = context;
+        }
+
+        public Sponsor GetByIdWithRewars(int id)
+        {
+            Sponsor? sponsorWithRewards = context.Sponsors.Include(s=>s.Rewards).FirstOrDefault(s=>s.Id == id);
+            return sponsorWithRewards;
+        }
     }
 }
